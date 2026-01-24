9b9025e357a3c6b53177b5f2bc8196249829eea6
Erbatax <samlarose1997@hotmail.ca>
2026-01-24 13:21:10 -0500

    Admin api section

--- main.go
+++ main.go
@@ -22,8 +22,8 @@ func main() {
 
 	serveMux.HandleFunc("GET /api/healthz", healthzHandler)
 
-	serveMux.HandleFunc("GET /api/metrics", apiCfg.hitsHandler)
-	serveMux.HandleFunc("POST /api/reset", apiCfg.metricsResetHandler)
+	serveMux.HandleFunc("GET /admin/metrics", apiCfg.hitsHandler)
+	serveMux.HandleFunc("POST /admin/reset", apiCfg.metricsResetHandler)
 
 	server.ListenAndServe()
 }
@@ -36,8 +36,13 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) hitsHandler(w http.ResponseWriter, r *http.Request) {
-	message := fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	message := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(message))
 }
