cc2cac07c53ae5d709424f55d7f9838989da7058
Erbatax <samlarose1997@hotmail.ca>
2026-01-24 13:15:21 -0500

    Separate api

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	serveMux := http.NewServeMux()
@@ -8,8 +16,38 @@ func main() {
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
+	apiCfg := &apiConfig{}
 
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+
+	serveMux.HandleFunc("GET /api/healthz", healthzHandler)
+
+	serveMux.HandleFunc("GET /api/metrics", apiCfg.hitsHandler)
+	serveMux.HandleFunc("POST /api/reset", apiCfg.metricsResetHandler)
 
 	server.ListenAndServe()
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) hitsHandler(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(message))
+}
+
+func (cfg *apiConfig) metricsResetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
